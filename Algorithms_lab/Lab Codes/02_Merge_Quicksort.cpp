51be792098932d200bb2c1add780a50a75ce0683
krishana dave <krishnadave671@gmail.com>
2022-01-28 18:19:24 +0530

    updated

--- Algorithms_lab/Lab Codes/02_Merge_Quicksort.cpp
+++ Algorithms_lab/Lab Codes/02_Merge_Quicksort.cpp
@@ -7,7 +7,7 @@ void swap(int *a , int *b){
     *b = temp;
 }
 int partition(int *arr, int l , int r){
-    int pivot = arr[l];
+    int pivot = arr[r];
     int i = l - 1;    
     for(int j = l; j < r ; j++){
         if(arr[j] < pivot){   
