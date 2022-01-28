94a7089a665977ef1eb6c6b24a971cbce29962fc
krishana dave <krishnadave671@gmail.com>
2022-01-28 18:17:31 +0530

    ignore files

--- Algorithms_lab/Lab Codes/02_Merge_Quicksort.cpp
+++ Algorithms_lab/Lab Codes/02_Merge_Quicksort.cpp
@@ -7,10 +7,10 @@ void swap(int *a , int *b){
     *b = temp;
 }
 int partition(int *arr, int l , int r){
-    int pivot = arr[r];
+    int pivot = arr[l];
     int i = l - 1;    
     for(int j = l; j < r ; j++){
-        if(arr[j] < pivot){
+        if(arr[j] < pivot){   
             i++;
             swap(&arr[i], &arr[j]);
         }
